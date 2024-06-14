995b8fb3c975b06685c7eae34f2c4b5d07fa9a65
bryancastillo10 <bryanangelocastillo.10@gmail.com>
2024-06-15 06:55:01 +0800

    reservationFunctionality

--- src/app/actions/index.ts
+++ src/app/actions/index.ts
@@ -1,5 +1,9 @@
 import getCurrentUser from "./getCurrentUser";
 import getListings from "./getListings";
 import getListingById from "./getListingById";
+import getReservations from "./getReservations";
 
-export { getCurrentUser, getListings, getListingById};
+export {
+    getCurrentUser, getListings,
+    getListingById, getReservations
+};
